c5edb9e917ca9ba835edf4c157f6b58553e581dd
Aayu-shh <aayush28.official@gmail.com>
2024-03-26 11:35:18 +0530

    Test Jenkins Polling 2

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const morgan = require('morgan');
 const app = express();
 app.use(cors({credentials:true,origin:'*'}));
 
+//Test push --Remove
 //const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags:'a'});
 
 //app.use(morgan('combined',{stream:accessLogStream}));
